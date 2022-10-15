1ac4cf02ec22f7636817cb8d850a5187bb70915a
unknown <swastik.dolas@ksolves.com>
2022-10-15 17:02:57 +0530

    added splash screen

--- social/App.js
+++ social/App.js
@@ -3,18 +3,15 @@ import { View } from 'react-native';
 import { WelcomeScreen, Login, Register, EnterOtp } from "./AppFolders/ModuleExports/index"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from './AppFolders/Components/SplashScreen';
-// hello
+import SplashScreen from "react-native-splash-screen"
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const [isVisible, setVisible] = useState(true);     // isVisible - if true show splash screen
 
   useEffect(() => {                         // show splash screen for some time 
-    const timer = setTimeout(() => {
-      setVisible(false)
-    }, 4600);
-    return () => clearTimeout(timer);
+    SplashScreen.hide()
   }, [])
 
 
@@ -23,32 +20,32 @@ const App = () => {
   //   return (<SplashScreen />)
 
   // if (!isVisible)
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Welcome" component={WelcomeScreen}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen name="login" component={Login}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen name="register" component={Register}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen name="enterOtp" component={EnterOtp}
-            options={{
-              headerShown: false
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen name="login" component={Login}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen name="register" component={Register}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen name="enterOtp" component={EnterOtp}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 
--- social/android/app/src/main/java/com/social/MainApplication.java
+++ social/android/app/src/main/java/com/social/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.social.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+// import org.devio.rn.splashscreen.SplashScreenReactPackage;// changed
 
 public class MainApplication extends Application implements ReactApplication {
 
